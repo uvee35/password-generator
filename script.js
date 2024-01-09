c9ca079c4a2184b57f149f59db3df0fec5dfba75
Victor <victormarquesrina@hotmail.com>
2024-01-09 14:38:20 +0000

    Add event handling and user interface code

--- script.js
+++ script.js
@@ -47,6 +47,21 @@ function generatePassword() {
 
   return finalPassword;
 }
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+
 
 
 
